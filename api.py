5ef1d560f57ee45a99cd9a5b739ca171285f6c50
='chrysanthus <='chrysanthus48akash@gmail.com>
2021-02-13 17:48:49 +0100

    up

--- api.py
+++ api.py
@@ -10,14 +10,22 @@ archive = response.json()
 seasons = 6
 max_ep={1:8, 2:10, 3:10, 4:10, 5:8, 6:7} #maximum episodes per season
 #breakpoint()
+
 def jprint(obj):
     # create a formatted string of the Python JSON object
     text = json.dumps(obj, sort_keys=True, indent=4)
-    #breakpoint()
+    #breakpoit()
     return (text)
+def season_error():
+    return f'<h3>Silicon valley has only {seasons} seasons </h3>'
+def maxep_error(s):
+    return f'<h3>Season {int(s)} of silicon valley has only {max_ep[int(s)]} episodes </h3>'
+def display(res):
+    if len(res) == 0:
+        return '<h3>No results found</h3>'
+    else:
+        return jsonify(res)
 
-#def season_error():
-#    return f'<h3>Silicon valley has only {seasons} seasons </h3>'
 
 @app.route('/', methods=['GET'])
 def home():
@@ -26,8 +34,8 @@ def home():
 
 @app.route('/siliconvalley/all', methods=['GET'])
 def silicon_valley():
-    #return(jprint(archive['_embedded']))
-    return (archive)
+    return(jprint(archive['_embedded']))
+
 
 @app.route('/siliconvalley/episodes/title', methods=['GET'])
 def title():
@@ -35,18 +43,24 @@ def title():
     s_no = request.args.get('s', None)
     ep_no  = request.args.get('e', None)
     title = request.args.get('name',None)
-    if ep_no is not None and s_no is not None and (s_no < 6) and (ep_no > max_ep[s_no]):
-        return f'<h3>Season {s_no} of silicon valley has only {max_ep[s_no]} episodes </h3>'
+    #breakpoint()
+    if s_no is not None and (int(s_no)>6):
+        return season_error()
+    if ep_no is not None and s_no is not None and (int(s_no)<= 6) and (int(ep_no) > max_ep[int(s_no)]):
+        return maxep_error(s_no)
+    else:
+        if ep_no is not None and s_no is not None:
+            for episode in archive['_embedded']['episodes']:
+                if episode['season']==int(s_no) and episode['number']==int(ep_no):
+                    results.append(episode)
     if title is not None :
-    #if 'name' in request.args:
         for episode in archive['_embedded']['episodes']:
             if episode['name']==title:
                 results.append(episode)
 
-
-    return jsonify(results)
-
     #breakpoint()
+    return display(results)
+
 
 @app.route('/siliconvalley/episodes/count', methods=['GET'])
 def count():
@@ -57,11 +71,12 @@ def count():
             ep_count.append(episode['name'])
     else:
         if int(s_no) > 6:
-            return f'<h3>Silicon valley has only {seasons} seasons </h3>'
+            return season_error();
+
         else:
             for episode in archive['_embedded']['episodes']:
                 if(episode['season']== int(s_no)):
                     ep_count.append(episode['name'])
     #breakpoint()
-    return jsonify(ep_count)
+    return display(ep_count)
 app.run()
